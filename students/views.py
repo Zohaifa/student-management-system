511da8771c2c66f3baa9f86bd94ed0ff76b72ffa
Zohaifa <ultimatezrage@gmail.com>
2024-12-05 18:58:56 +0600

    static added

--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
+from .models import Students
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'students/index.html')
+    return render(request, 'students/index.html', {'Students': Students.objects.all() })
+
