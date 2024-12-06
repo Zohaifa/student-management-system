1f7e40a1084d33531e55c00033409fe0fefc01f4
Zohaifa <ultimatezrage@gmail.com>
2024-12-06 17:35:32 +0600

    Student view modal created

--- students/urls.py
+++ students/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:id>', views.view_students, name='view_students'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Students
 from django.shortcuts import render
 
@@ -5,3 +7,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'students/index.html', {'Students': Students.objects.all() })
 
+def view_students(request, id):
+    student = Students.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
