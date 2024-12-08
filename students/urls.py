0cc593cbfd172c4e1fb71ec8d2ed4cc7beeea4f9
Zohaifa <ultimatezrage@gmail.com>
2024-12-08 07:57:27 +0600

    delete functionality added

--- students/urls.py
+++ students/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>', views.view_students, name='view_students'),
     path('add/', views.add, name='add'),
-    path('edit/<int:id>/', views.edit, name="edit")
+    path('edit/<int:id>/', views.edit, name="edit"),
+    path('delete/<int:id>/', views.delete, name="delete"),
 ]
